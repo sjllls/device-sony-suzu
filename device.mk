d066eec4b314ad7668c83a98815775fedd863b1e
sjllls <2012ji123@gmail.com>
2019-04-28 19:06:45 +0800

    Remove thermanager

--- device.mk
+++ device.mk
@@ -30,10 +30,6 @@ PRODUCT_COPY_FILES := \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/vendor/etc/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
-# Thermal Configuration
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/vendor/etc/thermanager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/thermanager.xml
-
 # Camera Configuration
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/vendor/etc/camera/camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_config.xml \
